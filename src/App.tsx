5d6c88552b18c8d573a35fc67a44a4d81ea3cbd2
Timóteo Reinheimer Piano <timoteopiano@gmail.com>
2022-05-03 18:51:59 -0300

    Add react-clear-cache

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import usePersistedState from './utils/usePersistedState'
 
 import GlobalStyle from './styles/global'
 
+import { useClearCacheCtx } from 'react-clear-cache'
+
 //Import Themes
 import light from './styles/themes/light'
 import dark from './styles/themes/dark'
@@ -18,12 +20,21 @@ function App() {
     setTheme(theme.title === 'light' ? dark : light)
   }
 
+  const { isLatestVersion, emptyCacheStorage } = useClearCacheCtx()
+
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
         <GlobalStyle />
-  
-        <Header toggleTheme={toggleTheme}/> 
+        <Header toggleTheme={toggleTheme}/>
+        {!isLatestVersion && (
+          <p>
+            <a
+              href='#'
+              onClick={e => {e.preventDefault(); emptyCacheStorage()}}
+            >Update Version</a>
+          </p>
+        )} 
       </div>
     </ThemeProvider>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import { ClearCacheProvider } from "react-clear-cache";
+import ReactDOM from "react-dom";
 
-import App from './App';
+import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <ClearCacheProvider duration={1}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ClearCacheProvider>,
+  document.getElementById("root")
 );
